42612cd3fe8d9e18e9d8c868b71ee42ae6772bb7
Alexis Bravo Llovera <alexis.bravo@externos.bci.cl>
2022-03-12 11:44:55 -0300

    environment variables added to dynamic attributes. Dynamic attr set to home component

--- src/app/modules/content/components/home/home.component.ts
+++ src/app/modules/content/components/home/home.component.ts
@@ -1,9 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'tia-content-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  constructor() {
+    console.log(environment.shortName);
+  }
+
+  shortName: string = environment.shortName;
+  role: string = environment.role;
+  profession: string = environment.profession;
+  instagram: string = environment.instagram;
+  skype: string = environment.skype;
+  stackOverFlowProfile: string = environment.stackOverFlowProfile;
+  githubProfile: string = environment.githubProfile;
+  linkedInProfile: string = environment.linkedInProfile;
+  email: string = environment.email;
+  phone: string = environment.phone;
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  shortName: "Alexis Bravo",
+  profession: "Informatics Engineer",
+  birthdate: "1985-05-05",
+  currentCountry: "Chile",
+  role: "Senior Software Engineer",
+  email: "alexis.ve@gmail.com",
+  phone: "+56 9 3096 8492",
+  currentCompanyName: "Nisum",
+  currentCompanyWebSite: "https://www.nisum.com/",
+  currentCompanyDescription: "a leading global digital commerce firm headquartered in California, with services spanning digital strategy and transformation, insights and analytics, blockchain, business agility, and custom software development",
+  instagram: "thisisalexis1",
+  skype: "thisisalexis",
+  stackOverFlowProfile: "https://stackoverflow.com/users/6035392/thisisalexis",
+  githubProfile: "https://github.com/thisisalexis",
+  linkedInProfile: "https://www.linkedin.com/in/alexis-jose-bravo-llovera/"
 };
 
 /*
