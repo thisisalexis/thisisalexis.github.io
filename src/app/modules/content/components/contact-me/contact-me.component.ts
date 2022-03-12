02dbb1a844ff11fda831d60f54e726ca86c7781c
Alexis Bravo Llovera <alexis.bravo@externos.bci.cl>
2022-03-12 13:34:46 -0300

    contact information content page updated with dynamic attributes

--- src/app/modules/content/components/contact-me/contact-me.component.ts
+++ src/app/modules/content/components/contact-me/contact-me.component.ts
@@ -1,7 +1,29 @@
 import {Component} from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'tia-content-contact-me',
   templateUrl: 'contact-me.component.html'
 })
-export class ContactMeComponent {}
+export class ContactMeComponent {
+
+  email: string = environment.email;
+  phone: string = environment.phone;
+  currentCompany: string = environment.currentCompanyName;
+  currentCompanyWebSite: string = environment.currentCompanyWebSite;
+  currentCompanyDescription: string = environment.currentCompanyDescription;
+  currentCountry: string = environment.currentCountry;
+  githubProfile: string = environment.githubProfile;
+  linkedInProfile: string = environment.linkedInProfile;
+
+
+
+  role: string = environment.role;
+  profession: string = environment.profession;
+  instagram: string = environment.instagram;
+  skype: string = environment.skype;
+  stackOverFlowProfile: string = environment.stackOverFlowProfile;
+
+
+
+}
